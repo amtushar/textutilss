071cd67fd7c8b3953e418f02bf29a9bc4b6336d1
amtushar <93277636+amtushar@users.noreply.github.com>
2022-01-29 21:10:01 +0530

    text app

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -13,3 +13,22 @@ export default function Alert(props) {
     </div>
   )
 }
+
+
+{/* <div class="toggleTrackX_dm8H"><span class="toggleIcon_pHJ9">🌞</span></div> */}
+// .toggleTrackX_dm8H {
+//     right: 10px;
+//     width: 10px;
+// }
+// .toggleTrackCheck_mk7D, .toggleTrackX_dm8H {
+//     bottom: 0;
+//     height: 10px;
+//     margin: auto 0;
+//     top: 0;
+//     position: absolute;
+// }
+
+// .toggleChecked_a04y .toggleTrackCheck_mk7D, .toggleTrackX_dm8H, [data-theme=dark] .toggle_iYfV .toggleTrackCheck_mk7D {
+//     opacity: 1;
+//     transition: opacity .25s;
+// }
